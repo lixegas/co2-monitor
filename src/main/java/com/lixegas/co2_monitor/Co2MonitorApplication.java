6b6e7cc65cadebe868f253cd69021d253a7cb6a0
Lixegas <167356473+lixegas@users.noreply.github.com>
2025-03-12 16:32:49 +0100

    first commit

---
+++ src/main/java/com/lixegas/co2_monitor/Co2MonitorApplication.java
@@ -0,0 +1,13 @@
+package com.lixegas.co2_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2MonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2MonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lixegas/co2_monitor/Co2MonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lixegas.co2_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2MonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
