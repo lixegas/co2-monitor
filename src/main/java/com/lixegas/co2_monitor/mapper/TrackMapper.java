c8304f3354b49f7dcf6c0badc682b736578ad28e
lixegas <167356473+lixegas@users.noreply.github.com>
2025-03-14 12:33:15 +0100

    Added mapper for track model

---
+++ src/main/java/com/lixegas/co2_monitor/mapper/TrackMapper.java
@@ -0,0 +1,9 @@
+package com.lixegas.co2_monitor.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Named;
+
+@Named("trackMapper")
+@Mapper(componentModel = "spring")
+public interface TrackMapper {
+}
--- src/main/java/com/lixegas/co2_monitor/model/dto/TrackDTO.java
+++ src/main/java/com/lixegas/co2_monitor/model/dto/TrackDTO.java
@@ -1,6 +1,7 @@
 package com.lixegas.co2_monitor.model.dto;
 
 
+import com.lixegas.co2_monitor.model.Sensor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,5 @@ public class TrackDTO {
     private Long id;
     private Double co2Level;
     private String createdAt;
-    private Long sensorId;
+    private Sensor sensor;
 }
